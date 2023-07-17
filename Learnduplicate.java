662f2d07a8deb414a8ca418f822d31f8ac5e72db
vivekam15 <139644776+vivekam15@users.noreply.github.com>
2023-07-17 12:11:41 +0530

    Learnduplicate.java

--- Learnduplicate.java
+++ Learnduplicate.java
@@ -6,8 +6,8 @@ public class Learnduplicate {
 		// TODO Auto-generated method stub
 		int[] num = {2,5,7,7,5,9,2,3};
 		for (int i = 0; i < num.length; i++) {
-			for (int j =i+1; j < num.length; j++) {
-				if(num[i]==num[i]) {		
+			for (int j =i+1; j < (num.length); j++) {
+				if(num[i]==num[j]) {		
 					System.out.println("Duplicate numbers:"+num[i]);
 
 				
