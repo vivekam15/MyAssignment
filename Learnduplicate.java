f584a71e2e753be2b6064ddf5969286682ac897e
vivekam15 <139644776+vivekam15@users.noreply.github.com>
2023-07-17 12:01:40 +0530

    Add files via upload

---
+++ Learnduplicate.java
@@ -0,0 +1,23 @@
+package week1.day2;
+
+public class Learnduplicate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] num = {2,5,7,7,5,9,2,3};
+		for (int i = 0; i < num.length; i++) {
+			for (int j =i+1; j < num.length; j++) {
+				if(num[i]==num[i]) {		
+					System.out.println("Duplicate numbers:"+num[i]);
+
+				
+				}
+			
+				
+			}
+
+		}
+
+	}
+
+}
